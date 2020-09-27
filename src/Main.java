c6993762d596faf8b191c8a53cc1f022602d86a6
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-09-27 16:43:58 +0530

    interfaces dummy created

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = DBConnection.getConnection();
-        
+
+        String x = "cvv";
+
         String queary = "INSERT INTO users(nic , name) VALUES (? , ?)";
 
         try {
